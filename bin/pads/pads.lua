fff8c04054284337f360a6dce03e5b15025a3dd2
Matías Israelson <57065102+israpps@users.noreply.github.com>
2022-12-03 23:38:18 -0300

    sync changes

--- bin/pads/pads.lua
+++ bin/pads/pads.lua
@@ -27,19 +27,19 @@ local REGION = KELFBinder.getsystemregion()
 local REGIONSTR = KELFBinder.getsystemregionString(REGION)
 
 Language = KELFBinder.getsystemLanguage()
-function promptkeys(SELECT, ST, CANCEL, CT, REFRESH, RT)
+function promptkeys(SELECT, ST, CANCEL, CT, REFRESH, RT, ALFA)
 
   if SELECT == 1 then
-    Graphics.drawImage(cross, 80.0, 400.0)
-    Font.ftPrint(font, 110 , 407, 0, 400, 16, ST)
+    Graphics.drawImage(cross, 80.0, 400.0, Color.new(0x80, 0x80, 0x80, 0x80-ALFA))
+    Font.ftPrint(font, 110 , 407, 0, 400, 16, ST, Color.new(0x80, 0x80, 0x80, 0x80-ALFA))
   end
   if CANCEL == 1 then
-    Graphics.drawImage(circle, 170.0, 400.0)
-    Font.ftPrint(font, 200 , 407, 0, 400, 16, CT)
+    Graphics.drawImage(circle, 170.0, 400.0, Color.new(0x80, 0x80, 0x80, 0x80-ALFA))
+    Font.ftPrint(font, 200 , 407, 0, 400, 16, CT, Color.new(0x80, 0x80, 0x80, 0x80-ALFA))
   end
   if REFRESH == 1 then
-    Graphics.drawImage(triangle, 260.0, 400.0)
-    Font.ftPrint(font, 290 , 407, 0, 400, 16, RT)
+    Graphics.drawImage(triangle, 260.0, 400.0, Color.new(0x80, 0x80, 0x80, 0x80-ALFA))
+    Font.ftPrint(font, 290 , 407, 0, 400, 16, RT, Color.new(0x80, 0x80, 0x80, 0x80-ALFA))
   end
 
 end
@@ -50,7 +50,7 @@ function greeting()
   local W = 1
     while CONTINUE do
       Screen.clear()
-      if Q > 250 then W = -1 end
+      if Q > 250 then W = -2 end
       if Q > 3 then Q = Q+W else CONTINUE = false end
       Font.ftPrint(font, 320, 20  , 8, 600, 16, "HELLO MOTHERFUCKER", Color.new(128, 128, 128, Q))
       Font.ftPrint(font, 320, 320 , 8, 600, 16, "Coded By El_isra (aka: Matias Israelson)", Color.new(128, 128, 128, Q))
@@ -63,47 +63,50 @@ function greeting()
 end
 
 function MainMenu()
-  local T = 3
-  local D = 0
+  local T = 1
+  local D = 15
+  local A = 0x50
   while true do
     Screen.clear()
-    Font.ftPrint(font, 150, 20,  0, 400, 32, "HELLO MOTHERFUCKER")
-    if T == 3 then
-      Font.ftPrint(font, 100, 150, 0, 400, 16, "Manage System Updates", Color.new(200, 200, 200, 0x80))
-    else
-      Font.ftPrint(font, 100, 150, 0, 400, 16, "Manage System Updates", Color.new(200, 200, 200, 0x50))
+    Font.ftPrint(font, 150, 20,  0, 400, 32, "Welcome to KELFBinder", Color.new(220, 220, 220, 0x80-A))
+    if T == 1 then
+      Font.ftPrint(font, 100, 150, 0, 400, 16, "Manage System Updates", Color.new(200, 200, 200, 0x80-A)) else
+      Font.ftPrint(font, 100, 150, 0, 400, 16, "Manage System Updates", Color.new(200, 200, 200, 0x50-A))
     end
     if T == 2 then
-      Font.ftPrint(font, 100, 190, 0, 400, 16, "Manage DVDPlayer Updates (Comming soon)", Color.new(200, 200, 200, 0x80))
-    else
-      Font.ftPrint(font, 100, 190, 0, 400, 16, "Manage DVDPlayer Updates", Color.new(200, 200, 200, 0x50))
+      Font.ftPrint(font, 100, 190, 0, 400, 16, "Manage DVDPlayer Updates (Comming soon)", Color.new(200, 200, 200, 0x80-A)) else
+      Font.ftPrint(font, 100, 190, 0, 400, 16, "Manage DVDPlayer Updates (Comming soon)", Color.new(200, 200, 200, 0x50-A))
     end
-    if T == 1 then
-      Font.ftPrint(font, 100, 300, 0, 400, 16, "Exit program", Color.new(200, 200, 200, 0x80))
-    else
-      Font.ftPrint(font, 100, 300, 0, 400, 16, "Exit program", Color.new(200, 200, 200, 0x50))
+    if T == 3 then
+      Font.ftPrint(font, 100, 230, 0, 400, 16, "System Information", Color.new(200, 200, 200, 0x80-A)) else
+      Font.ftPrint(font, 100, 230, 0, 400, 16, "System Information", Color.new(200, 200, 200, 0x50-A))
     end
-
-    promptkeys(1,"Select",0,0)
+    if T == 4 then
+      Font.ftPrint(font, 100, 300, 0, 400, 16, "Exit program", Color.new(200, 200, 200, 0x80-A)) else
+      Font.ftPrint(font, 100, 300, 0, 400, 16, "Exit program", Color.new(200, 200, 200, 0x50-A))
+    end
+    if A > 0 then A=A-1 end
+    promptkeys(1,"Select",0,0,0,0,A)
     Screen.flip()
     local pad = Pads.get()
 
     if Pads.check(pad, PAD_CROSS) and D == 0 then
+      D = 1
       Screen.clear()
       break
     end
 
     if Pads.check(pad, PAD_UP) and D == 0 then
-      T = T+1
+      T = T-1
       D = 1
     elseif Pads.check(pad, PAD_DOWN) and D == 0 then
-      T = T-1
+      T = T+1
       D = 1
     end
     if D > 0 then D = D+1 end
     if D > 10 then D = 0 end
-    if T < 1 then T = 3 end
-    if T > 3 then T = 1 end
+    if T < 1 then T = 4 end
+    if T > 4 then T = 1 end
 
   end
   return T
@@ -111,41 +114,48 @@ end
 
 function Installmodepicker()
   local T = 3
-  local D = 0
+  local D = 15
+  local A = 0x50
+  local PROMTPS = {
+    "Perform an installation compatible with this console\n and similar units",
+    "The clasic installation modes of FreeMcBoot",
+    "Choose manually wich updates you will install"}
   while true do
     Screen.clear()
-    Font.ftPrint(font, 150, 20,  0, 400, 32, "HELLO MOTHERFUCKER")
+    Font.ftPrint(font, 150, 20,  0, 400, 32, "Choose Installation mode", Color.new(220, 220, 220, 0x80-A))
 
-    if T == 3 then
-      Font.ftPrint(font, 100, 150, 0, 400, 16, "Normal Install", Color.new(200, 200, 200, 0x80))
+    if T == 1 then
+      Font.ftPrint(font, 100, 150, 0, 400, 16, "Normal Install", Color.new(200, 200, 200, 0x80-A))
     else
-      Font.ftPrint(font, 100, 150, 0, 400, 16, "Normal Install", Color.new(200, 200, 200, 0x50))
+      Font.ftPrint(font, 100, 150, 0, 400, 16, "Normal Install", Color.new(200, 200, 200, 0x50-A))
     end
     if T == 2 then
-      Font.ftPrint(font, 100, 190, 0, 400, 16, "Advanced Install (Comming soon!)", Color.new(200, 200, 200, 0x80)) else
-      Font.ftPrint(font, 100, 190, 0, 400, 16, "Advanced Install (Comming soon!)", Color.new(200, 200, 200, 0x50))
+      Font.ftPrint(font, 100, 190, 0, 400, 16, "Advanced Install (Comming soon!)", Color.new(200, 200, 200, 0x80-A)) else
+      Font.ftPrint(font, 100, 190, 0, 400, 16, "Advanced Install (Comming soon!)", Color.new(200, 200, 200, 0x50-A))
     end
-    if T == 1 then
-      Font.ftPrint(font, 100 , 230, 0, 400, 16, "Expert Install", Color.new(200, 200, 200, 0x80))
+    if T == 3 then
+      Font.ftPrint(font, 100 , 230, 0, 400, 16, "Expert Install", Color.new(200, 200, 200, 0x80-A))
     else
-      Font.ftPrint(font, 100 , 230, 0, 400, 16, "Expert Install", Color.new(200, 200, 200, 0x50))
+      Font.ftPrint(font, 100 , 230, 0, 400, 16, "Expert Install", Color.new(200, 200, 200, 0x50-A))
     end
-
-    promptkeys(1,"Select", 1, "Cancel",0)
-
+    
+    Font.ftPrint(font, 80 , 350, 0, 600, 32, PROMTPS[T], Color.new(200, 200, 200, 0x50-A))
+    promptkeys(1,"Select", 1, "Cancel",0, 0, A)
+    if A > 0 then A=A-1 end
     Screen.flip()
     local pad = Pads.get()
 
     if Pads.check(pad, PAD_CROSS) and D == 0 then
+      D = 1
       Screen.clear()
       break
     end
 
     if Pads.check(pad, PAD_UP) and D == 0 then
-      T = T+1
+      T = T-1
       D = 1
     elseif Pads.check(pad, PAD_DOWN) and D == 0 then
-      T = T-1
+      T = T+1
       D = 1
     end
     if D > 0 then D = D+1 end
@@ -181,39 +191,43 @@ end
 
 function MemcardPickup()
   local T = 0
-  local D = 0
+  local D = 15
   local Q = 0x77
   local QP = -4
+  local A = 0x50
   local mcinfo0 = System.getMCInfo(0)
   local mcinfo1 = System.getMCInfo(1)
   while true do
     local HC = ((mcinfo0.type == 2) or (mcinfo1.type == 2))
     Screen.clear()
-    Font.ftPrint(font, 320, 20,  8, 400, 32, "Choose a Memory card")
+    Font.ftPrint(font, 320, 20,  8, 400, 32, "Choose a Memory card", Color.new(0x80, 0x80, 0x80, 0x80-A))
     if mcinfo0.type == 2 then
-      Font.ftPrint(font, 80, 270,  0, 400, 32, "Memory Card 1\nFree Space: "..mcinfo0.freemem.." kb")
+      Font.ftPrint(font, 80, 270,  0, 400, 32, "Memory Card 1\nFree Space: "..mcinfo0.freemem.." kb", Color.new(0x80, 0x80, 0x80, 0x80-A))
       if T == 0 then
         Graphics.drawImage(MC, 80.0, 150.0, Color.new(0x80, 0x80, 0x80, Q))
       else
-        Graphics.drawImage(MC, 80.0, 150.0)
+        Graphics.drawImage(MC, 80.0, 150.0, Color.new(0x80, 0x80, 0x80, 0x80-A))
       end
     end
     if mcinfo1.type == 2 then
-      Font.ftPrint(font, 360, 270,  0, 400, 32, "Memory Card 2\nFree Space: "..mcinfo1.freemem.." kb")
+      Font.ftPrint(font, 360, 270,  0, 400, 32, "Memory Card 2\nFree Space: "..mcinfo1.freemem.." kb", Color.new(0x80, 0x80, 0x80, 0x80-A))
       if T == 1 then
         Graphics.drawImage(MC, 360.0, 150.0, Color.new(0x80, 0x80, 0x80, Q))
       else
-        Graphics.drawImage(MC, 360.0, 150.0)
+        Graphics.drawImage(MC, 360.0, 150.0, Color.new(0x80, 0x80, 0x80, 0x80-A))
       end
     end
-    promptkeys(1,"Select",1,"Cancel", 1,"Refresh")
+    if A > 0 then A=A-1 end
+    promptkeys(1,"Select",1,"Cancel", 1,"Refresh", A)
     Screen.flip()
     local pad = Pads.get()
     if Pads.check(pad, PAD_CROSS) and (D == 0) and (HC == true) then
+      D = 1
       Screen.clear()
       break
     end
 
+    if Pads.check(pad, PAD_CIRCLE) and D == 0 then break end
     if Pads.check(pad, PAD_LEFT) and D == 0 then
       T = 0
       D = 1
@@ -226,6 +240,7 @@ function MemcardPickup()
     if Pads.check(pad, PAD_TRIANGLE) and D == 0 then
       mcinfo0 = System.getMCInfo(0)
       mcinfo1 = System.getMCInfo(1)
+      A = 0x20
     end
 
     if Q < 4 then QP = 4 end
@@ -236,13 +251,15 @@ function MemcardPickup()
 
 
   end
+  Screen.clear()
   return T
 end
 
 function expertINSTprompt()
   local FINAL = 0
   local T = 0
-  local D = 0
+  local D = 15
+  local A = 0x40
   --[[
     JAP_ROM_100, JAP_ROM_101, JAP_ROM_120, JAP_STANDARD,
     USA_ROM_110, USA_ROM_120, USA_STANDARD,
@@ -266,53 +283,53 @@ function expertINSTprompt()
   while true do
     Screen.clear()
     Font.ftPrint(font, 150, 20,  0, 400, 32, string.format("Select system update executables %d %d %x",T,UPDT[T], FINAL))
-    Font.ftPrint(font, 100, 50,  0, 400, 16, "Japan - SCPH-XXX00", Color.new(250, 250, 250, 0x60))
-    Font.ftPrint(font, 100, 150, 0, 400, 16, "USA and Asia", Color.new(250, 250, 250, 0x60))
-    Font.ftPrint(font, 100, 230, 0, 400, 16, "Europe - SCPH-XXX0[2-4]", Color.new(250, 250, 250, 0x60))
-    Font.ftPrint(font, 100, 290, 0, 400, 16, "China - SCPH-XXX09", Color.new(250, 250, 250, 0x60))
-    Font.ftPrint(font, 20, 340, 0, 600, 32, UPDTT[T], Color.new(250, 250, 250, 0x40))
+    Font.ftPrint(font, 100, 50,  0, 400, 16, "Japan - SCPH-XXX00", Color.new(250, 250, 250, 0x80-A))
+    Font.ftPrint(font, 100, 150, 0, 400, 16, "USA and Asia", Color.new(250, 250, 250, 0x80))
+    Font.ftPrint(font, 100, 230, 0, 400, 16, "Europe - SCPH-XXX0[2-4]", Color.new(250, 250, 250, 0x80-A))
+    Font.ftPrint(font, 100, 290, 0, 400, 16, "China - SCPH-XXX09", Color.new(250, 250, 250, 0x80-A))
+    Font.ftPrint(font, 20, 340, 0, 600, 32, UPDTT[T], Color.new(250, 250, 250, 0x80-A))
     if T == JAP_ROM_100 then
-      Font.ftPrint(font, 110, 70, 0, 400, 16, "osdsys.elf", Color.new(200^(UPDT[0]+1), 200, 200, 0x80)) else
-      Font.ftPrint(font, 110, 70, 0, 400, 16, "osdsys.elf", Color.new(200^(UPDT[0]+1), 200, 200, 0x60))
+      Font.ftPrint(font, 110, 70, 0, 400, 16, "osdsys.elf", Color.new(200^(UPDT[0]+1), 200, 200, 0x80-A)) else
+      Font.ftPrint(font, 110, 70, 0, 400, 16, "osdsys.elf", Color.new(200^(UPDT[0]+1), 200, 200, 0x60-A))
     end
     if T == JAP_ROM_101 then
-      Font.ftPrint(font, 110, 90, 0, 400, 16, "osd110.elf", Color.new(200^(UPDT[1]+1), 200, 200, 0x80)) else
-      Font.ftPrint(font, 110, 90, 0, 400, 16, "osd110.elf", Color.new(200^(UPDT[1]+1), 200, 200, 0x60))
+      Font.ftPrint(font, 110, 90, 0, 400, 16, "osd110.elf", Color.new(200^(UPDT[1]+1), 200, 200, 0x80-A)) else
+      Font.ftPrint(font, 110, 90, 0, 400, 16, "osd110.elf", Color.new(200^(UPDT[1]+1), 200, 200, 0x60-A))
     end
     if T == JAP_ROM_120 then
-      Font.ftPrint(font, 110, 110, 0, 400, 16, "osd130.elf", Color.new(200^(UPDT[2]+1), 200, 200, 0x80)) else
-      Font.ftPrint(font, 110, 110, 0, 400, 16, "osd130.elf", Color.new(200^(UPDT[2]+1), 200, 200, 0x60))
+      Font.ftPrint(font, 110, 110, 0, 400, 16, "osd130.elf", Color.new(200^(UPDT[2]+1), 200, 200, 0x80-A)) else
+      Font.ftPrint(font, 110, 110, 0, 400, 16, "osd130.elf", Color.new(200^(UPDT[2]+1), 200, 200, 0x60-A))
     end
     if T == JAP_STANDARD then
-      Font.ftPrint(font, 110, 130, 0, 400, 16, "osdmain.elf", Color.new(200^(UPDT[3]+1), 200, 200, 0x80)) else
-      Font.ftPrint(font, 110, 130, 0, 400, 16, "osdmain.elf", Color.new(200^(UPDT[3]+1), 200, 200, 0x60))
+      Font.ftPrint(font, 110, 130, 0, 400, 16, "osdmain.elf", Color.new(200^(UPDT[3]+1), 200, 200, 0x80-A)) else
+      Font.ftPrint(font, 110, 130, 0, 400, 16, "osdmain.elf", Color.new(200^(UPDT[3]+1), 200, 200, 0x60-A))
     end
     if T == USA_ROM_110 then
-      Font.ftPrint(font, 110, 170, 0, 400, 16, "osd120.elf", Color.new(200^(UPDT[4]+1), 200, 200, 0x80)) else
-      Font.ftPrint(font, 110, 170, 0, 400, 16, "osd120.elf", Color.new(200^(UPDT[4]+1), 200, 200, 0x60))
+      Font.ftPrint(font, 110, 170, 0, 400, 16, "osd120.elf", Color.new(200^(UPDT[4]+1), 200, 200, 0x80-A)) else
+      Font.ftPrint(font, 110, 170, 0, 400, 16, "osd120.elf", Color.new(200^(UPDT[4]+1), 200, 200, 0x60-A))
     end
     if T == USA_ROM_120 then
-      Font.ftPrint(font, 110, 190, 0, 400, 16, "osd130.elf", Color.new(200^(UPDT[5]+1), 200, 200, 0x80)) else
-      Font.ftPrint(font, 110, 190, 0, 400, 16, "osd130.elf", Color.new(200^(UPDT[5]+1), 200, 200, 0x60))
+      Font.ftPrint(font, 110, 190, 0, 400, 16, "osd130.elf", Color.new(200^(UPDT[5]+1), 200, 200, 0x80-A)) else
+      Font.ftPrint(font, 110, 190, 0, 400, 16, "osd130.elf", Color.new(200^(UPDT[5]+1), 200, 200, 0x60-A))
     end
     if T == USA_STANDARD then
-      Font.ftPrint(font, 110, 210, 0, 400, 16, "osdmain.elf", Color.new(200^(UPDT[6]+1), 200, 200, 0x80)) else
-      Font.ftPrint(font, 110, 210, 0, 400, 16, "osdmain.elf", Color.new(200^(UPDT[6]+1), 200, 200, 0x60))
+      Font.ftPrint(font, 110, 210, 0, 400, 16, "osdmain.elf", Color.new(200^(UPDT[6]+1), 200, 200, 0x80-A)) else
+      Font.ftPrint(font, 110, 210, 0, 400, 16, "osdmain.elf", Color.new(200^(UPDT[6]+1), 200, 200, 0x60-A))
     end
     if T == EUR_ROM_120 then
-      Font.ftPrint(font, 110, 250, 0, 400, 16, "osd130.elf", Color.new(200^(UPDT[7]+1), 200, 200, 0x80)) else
-      Font.ftPrint(font, 110, 250, 0, 400, 16, "osd130.elf", Color.new(200^(UPDT[7]+1), 200, 200, 0x60))
+      Font.ftPrint(font, 110, 250, 0, 400, 16, "osd130.elf", Color.new(200^(UPDT[7]+1), 200, 200, 0x80-A)) else
+      Font.ftPrint(font, 110, 250, 0, 400, 16, "osd130.elf", Color.new(200^(UPDT[7]+1), 200, 200, 0x60-A))
     end
     if T == EUR_STANDARD then
-      Font.ftPrint(font, 110, 270, 0, 400, 16, "osdmain.elf", Color.new(200^(UPDT[8]+1), 200, 200, 0x80)) else
-      Font.ftPrint(font, 110, 270, 0, 400, 16, "osdmain.elf", Color.new(200^(UPDT[8]+1), 200, 200, 0x60))
+      Font.ftPrint(font, 110, 270, 0, 400, 16, "osdmain.elf", Color.new(200^(UPDT[8]+1), 200, 200, 0x80-A)) else
+      Font.ftPrint(font, 110, 270, 0, 400, 16, "osdmain.elf", Color.new(200^(UPDT[8]+1), 200, 200, 0x60-A))
     end
     if T == CHN_STANDARD then
-      Font.ftPrint(font, 110, 310, 0, 400, 16, "osdmain.elf", Color.new(200^(UPDT[9]+1), 200, 200, 0x80)) else
-      Font.ftPrint(font, 110, 310, 0, 400, 16, "osdmain.elf", Color.new(200^(UPDT[9]+1), 200, 200, 0x60))
+      Font.ftPrint(font, 110, 310, 0, 400, 16, "osdmain.elf", Color.new(200^(UPDT[9]+1), 200, 200, 0x80-A)) else
+      Font.ftPrint(font, 110, 310, 0, 400, 16, "osdmain.elf", Color.new(200^(UPDT[9]+1), 200, 200, 0x60-A))
     end
-
-    promptkeys(1,"Select",1,"Cancel",1,"Begin Installation")
+    if A > 0 then A=A-1 end
+    promptkeys(1,"Select",1,"Cancel",1,"Begin Installation", A)
     Screen.flip()
     local pad = Pads.get()
     if UPDT[0] == 1 or UPDT[1] == 1 and UPDT[2] == 0 then
@@ -320,6 +337,7 @@ function expertINSTprompt()
     end
 
     if Pads.check(pad, PAD_CROSS) and D == 0 then
+      D = 1
       if UPDT[T] == 1 then UPDT[T] = 0 else UPDT[T] = 1 end
       if T == 2 and UPDT[2] == 0 then
         UPDT[0] =0
@@ -328,6 +346,7 @@ function expertINSTprompt()
     end
 
     if Pads.check(pad, PAD_TRIANGLE) and D == 0 then
+      D = 1
       break
     end
 
@@ -345,6 +364,7 @@ function expertINSTprompt()
     if T > CHN_STANDARD then T = JAP_ROM_100 end
 
   end
+  Screen.clear()
   return UPDT
 end
 
@@ -400,23 +420,67 @@ function performExpertINST(port, slot, UPDT)
   Screen.flip()
 end
 
+function Ask2quit()
+  Screen.clear()
+  Font.ftPrint(font, 320, 240  , 8, 600, 16, "Exit Program?")
+  promptkeys(1," Yes",1," No", 1,"Run wLaunchELF", 0)
+  Screen.flip()
+  while true do
+    
+    local pad = Pads.get()
+    if Pads.check(pad, PAD_CROSS) then System.exitToBrowser() end
+    if Pads.check(pad, PAD_CIRCLE) then break end
+    if Pads.check(pad, PAD_TRIANGLE) then System.loadELF("INSTALL/CORE/BACKDOOR.ELF") end
+  end
+end
+
+function SystemInfo()
+  local D = 15
+  local A = 0x50
+  while true do
+    Screen.clear()
+    Font.ftPrint(font, 320, 20,  8, 400, 32, "SYSTEM INFORMATION", Color.new(220, 220, 220, 0x80-A))
+    Font.ftPrint(font, 50, 60,  0, 400, 32, string.format("ROMVER = [%s]", ROMVER), Color.new(220, 220, 220, 0x80-A))
+    Font.ftPrint(font, 50, 80,  0, 600, 32, string.format("System Update Path = [%s]", KELFBinder.calculateSysUpdatePath()), Color.new(220, 220, 220, 0x80-A))
+
+    promptkeys(0,"Select", 1, "Quit",0, 0, A)
+    if A > 0 then A=A-1 end
+    Screen.flip()
+    local pad = Pads.get()
+    if D > 0 then D = D+1 end
+    if D > 10 then D = 0 end
+    if Pads.check(pad, PAD_CIRCLE) and D == 0 then break end
+  end
+end
 -- SCRIPT BEHAVIOUR BEGINS --
+--SystemInfo()
+greeting()
+while true do
 local TT = MainMenu()
+System.sleep(1)
+-- SYSTEM UPDATE
 if (TT == 1) then
   local TTT = Installmodepicker()
-  if TTT == 3 
-  then 
+  System.sleep(1)
+  if TTT == 1 then
     NormalInstall(MemcardPickup(),0)
-  elseif TTT == 1 then
+    System.sleep(1)
+  elseif TTT == 3 then
     local port = MemcardPickup()
     local UPDT = expertINSTprompt()
     performExpertINST(port,0,UPDT)
+    System.sleep(1)
   end
-end
-while true do end
-NormalInstall(MemcardPickup(),0)
- --]]
+elseif TT == 2 then
 
+elseif TT == 3 then
+  SystemInfo()
+elseif TT == 4 then
+  Ask2quit()
+  System.sleep(1)
+end
+-- SYSTEM UPDATE
+end
 Screen.clear()
 while true do end
 
