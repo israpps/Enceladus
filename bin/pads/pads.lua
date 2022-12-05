82509d6359c055d09f5c635c9074622d1688190f
Matías Israelson <57065102+israpps@users.noreply.github.com>
2022-12-05 20:13:01 -0300

    push failed 3d attempt
    3d stuff is rendered on top of 2d stuff
    
    will leave this abandoned untill a workaround is found

--- bin/pads/pads.lua
+++ bin/pads/pads.lua
@@ -1,3 +1,20 @@
+-- 3D STUFF
+Screen.setMode(NTSC, 640, 448, CT24, INTERLACED, FIELD, true, Z16S)
+Render.init(4/3)
+local orangetex = Graphics.loadImage("mesh/CUBE4.png")
+Graphics.setImageFilters(orangetex, LINEAR)
+local CUBE = Render.loadOBJ("mesh/QBO.obj", orangetex)
+Camera.position(0.0, 0.0, 50.0)
+Camera.rotation(0.0, 0.0,  0.0)
+Lights.create(4)
+local savedlx = 0.0
+local savedly = 180.0
+local savedrx = 9.5
+local savedry = -4.5
+local modelz = 0.0
+local lxmod = -0.02
+local lymod = 0.02
+-- 3D STUFF
 Screen.clear()
 Font.ftInit()
 font = Font.ftLoad("pads/font.ttf")
@@ -26,8 +43,31 @@ local MC = Graphics.loadImage("pads/MC.png")
 
 local REGION = KELFBinder.getsystemregion()
 local REGIONSTR = KELFBinder.getsystemregionString(REGION)
-
+local RR = 0.1
 Language = KELFBinder.getsystemLanguage()
+
+function DealWith3D()
+  Lights.set(1,  0.0,  0.0,  0.0, 0.5*RR, 0.5*RR, 0.5*RR,     AMBIENT)
+  Lights.set(2,  1.0,  0.0, -1.0, 0.0*RR, 0.0*RR, 0.0*RR, DIRECTIONAL)
+  Lights.set(3,  0.0,  1.0, -1.0, 0.9*RR, 0.5*RR, 0.5*RR, DIRECTIONAL)
+  Lights.set(4, -1.0, -1.0, -1.0, 0.5*RR, 0.5*RR, 0.5*RR, DIRECTIONAL)
+
+	if savedlx > 50 then
+		lxmod = -0.02
+	elseif savedlx < -50 then
+		lxmod = 0.02
+	end
+	if savedly > 50 then
+		lymod = -0.02
+	elseif savedly < -50 then
+		lymod = 0.02
+	end
+  savedlx = savedlx + lxmod
+  Render.drawOBJ(CUBE,  savedrx, savedry, modelz, savedly, savedlx, 0.0)
+  Render.drawOBJ(CUBE,  savedrx-5, savedry+1, modelz, savedly, -savedlx, 0.0)
+
+end
+
 function promptkeys(SELECT, ST, CANCEL, CT, REFRESH, RT, ALFA)
 
   if SELECT == 1 then
@@ -50,7 +90,9 @@ function greeting()
   local Q = 4
   local W = 1
     while CONTINUE do
+      --if RR < 1 then RR = RR+0.001 end
       Screen.clear()
+      --DealWith3D()
       if Q > 250 then W = -2 end
       if Q > 3 then Q = Q+W else CONTINUE = false end
       Font.ftPrint(font, 320, 20  , 8, 600, 16, "HELLO MOTHERFUCKER", Color.new(128, 128, 128, Q))
@@ -68,7 +110,9 @@ function MainMenu()
   local D = 15
   local A = 0x50
   while true do
+    if RR < 1 then RR = RR+0.011 end
     Screen.clear()
+    DealWith3D()
     Font.ftPrint(font, 150, 20,  0, 400, 32, "Welcome to KELFBinder", Color.new(220, 220, 220, 0x80-A))
     if T == 1 then
       Font.ftPrint(font, 100, 150, 0, 400, 16, "Manage System Updates", Color.new(200, 200, 200, 0x80-A)) else
@@ -123,6 +167,7 @@ function Installmodepicker()
     "Choose manually wich updates you will install"}
   while true do
     Screen.clear()
+    DealWith3D()
     Font.ftPrint(font, 150, 20,  0, 400, 32, "Choose Installation mode", Color.new(220, 220, 220, 0x80-A))
 
     if T == 1 then
@@ -175,6 +220,7 @@ function NormalInstall(port, slot)
   KELFBinder.setSysUpdateFoldProps(port, slot, KELFBinder.getsysupdatefolder())
   SYSUPDATEPATH = KELFBinder.calculateSysUpdatePath()
   Screen.clear(Color.new(0, 0, 0))
+  DealWith3D()
   Font.ftPrint(font, 320, 20  , 8, 600, 64, "BINDING KELF\n\n"..SYSUPDATEPATH.."\n")
   Screen.flip()
   if (ROMVERN == 100) or (ROMVERN == 101) then -- PROTOKERNEL NEED TWO UPDATES TO FUNCTION
@@ -207,6 +253,7 @@ function MemcardPickup()
   while true do
     local HC = ((mcinfo0.type == 2) or (mcinfo1.type == 2))
     Screen.clear()
+    DealWith3D()
     Font.ftPrint(font, 320, 20,  8, 400, 32, "Choose a Memory card", Color.new(0x80, 0x80, 0x80, 0x80-A))
     if mcinfo0.type == 2 then
       Font.ftPrint(font, 80, 270,  0, 400, 32, "Memory Card 1\nFree Space: "..mcinfo0.freemem.." kb", Color.new(0x80, 0x80, 0x80, 0x80-A))
@@ -291,6 +338,7 @@ function expertINSTprompt()
   end
   while true do
     Screen.clear()
+    DealWith3D()
     Font.ftPrint(font, 150, 20,  0, 400, 32, string.format("Select system update executables %d %d %x",T,UPDT[T], FINAL))
     Font.ftPrint(font, 100, 50,  0, 400, 16, "Japan - SCPH-XXX00", Color.new(250, 250, 250, 0x80-A))
     Font.ftPrint(font, 100, 150, 0, 400, 16, "USA and Asia", Color.new(250, 250, 250, 0x80))
@@ -380,6 +428,7 @@ end
 function secrerr(RET)
   if RET < 0 then
     Screen.clear(Color.new(0xff, 00, 00))
+    DealWith3D()
     Font.ftPrint(font, 320, 40,  8, 400, 64, string.format("Installation Failed! (%d)", RET))
     if RET == -5 then
       Font.ftPrint(font, 320, 60,  8, 400, 64, "I/O ERROR")
@@ -390,12 +439,14 @@ function secrerr(RET)
     else
       Font.ftPrint(font, 320, 40,  8, 400, 64, "Unknown error!")
     end
+    Screen.flip()
     while true do end
   end
 end
 
 function performExpertINST(port, slot, UPDT)
   Screen.clear()
+  DealWith3D()
   local FLAGS = 0
   for i=0,9 do
     if UPDT[i] == 1 then
@@ -446,11 +497,14 @@ function Ask2quit()
   promptkeys(1," Yes",1," No", 1,"Run wLaunchELF", 0)
   Screen.flip()
   while true do
-    
+    Screen.clear()
+    DealWith3D()
+
     local pad = Pads.get()
     if Pads.check(pad, PAD_CROSS) then System.exitToBrowser() end
     if Pads.check(pad, PAD_CIRCLE) then break end
     if Pads.check(pad, PAD_TRIANGLE) then System.loadELF("INSTALL/CORE/BACKDOOR.ELF") end
+    Screen.flip()
   end
 end
 
@@ -460,6 +514,7 @@ function SystemInfo()
   local UPDTPATH = KELFBinder.calculateSysUpdatePath()
   while true do
     Screen.clear()
+    DealWith3D()
     Font.ftPrint(font, 320, 20,  8, 400, 32, "SYSTEM INFORMATION", Color.new(220, 220, 220, 0x80-A))
     Font.ftPrint(font, 50, 60,  0, 400, 32, string.format("ROMVER = [%s]", ROMVER), Color.new(220, 220, 220, 0x80-A))
     Font.ftPrint(font, 50, 80,  0, 600, 32, string.format("System Update Path = [%s]", UPDTPATH), Color.new(220, 220, 220, 0x80-A))
