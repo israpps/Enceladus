israpps/Enceladus

commit 2a65e5d05ba010158060efbf831e5f73521c5fd6

report unformatted ps2 card and avoid inst on them

bin/INSTALL/KELFBinder.lua
@@ -569,8 +569,11 @@ function MemcardPickup()
     Font.ftPrint(font, 320, 20, 8, 630, 32, LNG_MEMCARD0, Color.new(0x80, 0x80, 0x80, 0x80 - A))
 
     if mcinfo0.type == 2 then
-      Font.ftPrint(font, 80, 270, 0, 630, 32, string.format(LNG_MEMCARD1, 1, mcinfo0.freemem),
-        Color.new(0x80, 0x80, 0x80, 0x80 - A))
+      if mcinfo0.format == 1 then
+        Font.ftPrint(font, 80, 270, 0, 630, 32, string.format(LNG_MEMCARD1, 1, mcinfo0.freemem), Color.new(0x80, 0x80, 0x80, 0x80 - A))
+      else
+        Font.ftPrint(font, 80, 270, 0, 630, 32, LNG_UNFORMATTED_CARD, Color.new(0x80, 0, 0, 0))
+      end
     elseif mcinfo0.type ~= 0 then
       Font.ftPrint(font, 360, 270, 0, 630, 32, LNG_INCOMPATIBLE_CARD, Color.new(0x80, 0x80, 0x80, 0x80 - A))
     end
@@ -581,8 +584,11 @@ function MemcardPickup()
       Graphics.drawScaleImage(mi0, 80.0 + 32, 180.0, 64, 64, Color.new(0x80, 0x80, 0x80, 0x80 - A))
     end
     if mcinfo1.type == 2 then
-      Font.ftPrint(font, 360, 270, 0, 630, 32, string.format(LNG_MEMCARD1, 2, mcinfo1.freemem),
-        Color.new(0x80, 0x80, 0x80, 0x80 - A))
+      if mcinfo1.format == 1 then
+      Font.ftPrint(font, 360, 270, 0, 630, 32, string.format(LNG_MEMCARD1, 2, mcinfo1.freemem), Color.new(0x80, 0x80, 0x80, 0x80 - A))
+    else
+      Font.ftPrint(font, 360, 270, 0, 630, 32, LNG_UNFORMATTED_CARD, Color.new(0x80, 0, 0, 0))
+    end
     elseif mcinfo1.type ~= 0 then
       Font.ftPrint(font, 360, 270, 0, 630, 32, LNG_INCOMPATIBLE_CARD, Color.new(0x80, 0x80, 0x80, 0x80 - A))
     end
@@ -601,7 +607,7 @@ function MemcardPickup()
       mcinfo1 = System.getMCInfo(1)
       A = 0x20
       D = 1
-      if (mcinfo0.type == 2 and T == 0) or (mcinfo1.type == 2 and T == 1) then
+      if (mcinfo0.type == 2 and T == 0 and mcinfo0.format == 1) or (mcinfo1.type == 2 and T == 1 and mcinfo1.format == 1) then
         Screen.clear()
         break
       end

bin/lang/spanish.lua
@@ -82,3 +82,4 @@
 	LNG_NOT_ENOUGH_SPACE1 = "Espacio requerido  %.1f Kb - Espacio disponible %.1f Kb"
 	LNG_INCOMPATIBLE_CARD = "Dispositivo incompatible!"
 	LNG_INSTALLING_EXTRA = "Instalando archivos adicionales..."
+	LNG_UNFORMATTED_CARD = "La Memory Card no está formateada"

etc/boot.lua
@@ -83,6 +83,7 @@ Screen.clear() Graphics.drawPixel(320, 224, 0x80808080) Screen.flip()
 	LNG_NOT_ENOUGH_SPACE1 = "Needed space  %.1f Kb - Available Space %.1f Kb"
 	LNG_INCOMPATIBLE_CARD = "This device is not compatible!"
 	LNG_INSTALLING_EXTRA = "Installing aditional files..."
+	LNG_UNFORMATTED_CARD = "Memory Card is not formatted"
 
 	BETANUM = "011"
 IS_NOT_PUBLIC_READY = true

bin/INSTALL/EXTINST.lua
@@ -14,6 +14,7 @@ EXTRA_INST_SRC = { -- CONTAINS SOURCE FILES FOR INSTALLATION
   "INSTALL/ASSETS/APPS/icon.sys",
   "INSTALL/ASSETS/APPS/OPNPS2LD.ELF"
 }
+
 EXTRA_INST_FOLDE = 3 -- SIZE OF THE EXTRA_INST_MKD TABLE
 EXTRA_INST_MKD = {
   "PS2BBL",

commit d63aa8cd610109ca1b88784d914063631af44cc4

fix bug crash caused by 2a65e5d and tweak text

bin/INSTALL/KELFBinder.lua
@@ -549,17 +549,13 @@ function MemcardPickup()
   local mcinfo1 = System.getMCInfo(1)
   while true do
     local HC = ((mcinfo0.type == 2) or (mcinfo1.type == 2))
-    if mcinfo0.type == 2 then
-      mi0 = MC2
-    elseif mcinfo0.type == 1 then
-      mi0 = MC1
+    if mcinfo0.type == 2 then mi0 = MC2
+    elseif mcinfo0.type == 1 then mi0 = MC1
     else mi0 = MCU
     end
 
-    if mcinfo1.type == 2 then
-      mi1 = MC2
-    elseif mcinfo1.type == 1 then
-      mi1 = MC1
+    if mcinfo1.type == 2 then mi1 = MC2
+    elseif mcinfo1.type == 1 then mi1 = MC1
     else mi1 = MCU
     end
 
@@ -569,13 +565,14 @@ function MemcardPickup()
     Font.ftPrint(font, 320, 20, 8, 630, 32, LNG_MEMCARD0, Color.new(0x80, 0x80, 0x80, 0x80 - A))
 
     if mcinfo0.type == 2 then
+      Font.ftPrint(font, 80 + 64, 270, 8, 630, 32, string.format(LNG_MEMCARD1, 1), Color.new(0x80, 0x80, 0x80, 0x80 - A))
       if mcinfo0.format == 1 then
-        Font.ftPrint(font, 80, 270, 0, 630, 32, string.format(LNG_MEMCARD1, 1, mcinfo0.freemem), Color.new(0x80, 0x80, 0x80, 0x80 - A))
+        Font.ftPrint(font, 80 + 64, 290, 8, 630, 32, string.format(LNG_MEMCARD2, mcinfo0.freemem), Color.new(0x80, 0x80, 0x80, 0x80 - A))
       else
-        Font.ftPrint(font, 80, 270, 0, 630, 32, LNG_UNFORMATTED_CARD, Color.new(0x80, 0, 0, 0))
+        Font.ftPrint(font, 80 + 64, 290, 8, 630, 32, LNG_UNFORMATTED_CARD, Color.new(0x80, 0, 0, 0x80-A))
       end
     elseif mcinfo0.type ~= 0 then
-      Font.ftPrint(font, 360, 270, 0, 630, 32, LNG_INCOMPATIBLE_CARD, Color.new(0x80, 0x80, 0x80, 0x80 - A))
+      Font.ftPrint(font, 80 + 64, 270, 8, 630, 32, LNG_INCOMPATIBLE_CARD, Color.new(0x80, 0x80, 0x80, 0x80 - A))
     end
 
     if T == 0 then
@@ -583,14 +580,16 @@ function MemcardPickup()
     else
       Graphics.drawScaleImage(mi0, 80.0 + 32, 180.0, 64, 64, Color.new(0x80, 0x80, 0x80, 0x80 - A))
     end
+
     if mcinfo1.type == 2 then
+      Font.ftPrint(font, 360 + 64, 270, 8, 630, 32, string.format(LNG_MEMCARD1, 2), Color.new(0x80, 0x80, 0x80, 0x80 - A))
       if mcinfo1.format == 1 then
-      Font.ftPrint(font, 360, 270, 0, 630, 32, string.format(LNG_MEMCARD1, 2, mcinfo1.freemem), Color.new(0x80, 0x80, 0x80, 0x80 - A))
-    else
-      Font.ftPrint(font, 360, 270, 0, 630, 32, LNG_UNFORMATTED_CARD, Color.new(0x80, 0, 0, 0))
-    end
+        Font.ftPrint(font, 360 + 64, 290, 8, 630, 32, string.format(LNG_MEMCARD2, mcinfo1.freemem), Color.new(0x80, 0x80, 0x80, 0x80 - A))
+      else
+        Font.ftPrint(font, 360 + 64, 290, 8, 630, 32, LNG_UNFORMATTED_CARD, Color.new(0x80, 0, 0, 0x80-A))
+      end
     elseif mcinfo1.type ~= 0 then
-      Font.ftPrint(font, 360, 270, 0, 630, 32, LNG_INCOMPATIBLE_CARD, Color.new(0x80, 0x80, 0x80, 0x80 - A))
+      Font.ftPrint(font, 360 + 64, 270, 0, 630, 32, LNG_INCOMPATIBLE_CARD, Color.new(0x80, 0x80, 0x80, 0x80 - A))
     end
     if T == 1 then
       Graphics.drawScaleImage(mi1, 360.0 + 32, 180.0, 64, 64, Color.new(0x90, 0x90, 0x90, Q))

bin/lang/spanish.lua
@@ -82,4 +82,4 @@
 	LNG_NOT_ENOUGH_SPACE1 = "Espacio requerido  %.1f Kb - Espacio disponible %.1f Kb"
 	LNG_INCOMPATIBLE_CARD = "Dispositivo incompatible!"
 	LNG_INSTALLING_EXTRA = "Instalando archivos adicionales..."
-	LNG_UNFORMATTED_CARD = "La Memory Card no está formateada"
+	LNG_UNFORMATTED_CARD = "sin formatear"

etc/boot.lua
@@ -25,7 +25,8 @@ Screen.clear() Graphics.drawPixel(320, 224, 0x80808080) Screen.flip()
 	LNG_INSTPMPT = "BINDING KELF\n\n%s\n"
 	LNG_INSTPMPT1 = "Installation finished!"
 	LNG_MEMCARD0 = "Choose a Memory Card"
-	LNG_MEMCARD1 = "Memory Card %d\nFree space %d kb"
+	LNG_MEMCARD1 = "Memory Card %d"
+	LNG_MEMCARD2 = "Free space %d kb"
 	LNG_SUC0 = "Kernel Patch for early SCPH-10000\nneeds SCPH-18000 update to function"
 	LNG_SUC1 = "Kernel Patch for late SCPH-10000 and SCPH-15000\nneeds SCPH-18000 update to function"
 	LNG_SUC2 = "SCPH-18000"
@@ -81,9 +82,9 @@ Screen.clear() Graphics.drawPixel(320, 224, 0x80808080) Screen.flip()
 	LNG_ERROR = "Error!"
 	LNG_NOT_ENOUGH_SPACE0 = "There is not enough space on the selected Memory Card"
 	LNG_NOT_ENOUGH_SPACE1 = "Needed space  %.1f Kb - Available Space %.1f Kb"
-	LNG_INCOMPATIBLE_CARD = "This device is not compatible!"
+	LNG_INCOMPATIBLE_CARD = "incompatible device!"
 	LNG_INSTALLING_EXTRA = "Installing aditional files..."
-	LNG_UNFORMATTED_CARD = "Memory Card is not formatted"
+	LNG_UNFORMATTED_CARD = "unformatted"
 
 	BETANUM = "011"
 IS_NOT_PUBLIC_READY = true