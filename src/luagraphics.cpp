98ac954c0f839de6b9bf20f95be006d1c64892ca
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-06-30 23:07:25 -0300

    embedded png

--- src/luagraphics.cpp
+++ src/luagraphics.cpp
@@ -102,7 +102,6 @@ static int lua_ftSetPixelSize(lua_State *L) {
 	return 0;
 }
 
-
 static int lua_ftSetCharSize(lua_State *L) {
 	if (lua_gettop(L) != 3) return luaL_error(L, "wrong number of arguments"); 
 	int fontid = luaL_checkinteger(L, 1);
@@ -135,7 +134,6 @@ static int lua_ftunload(lua_State *L){
 	fntRelease(fontid);
 	return 0;
 }
-
 
 static int lua_ftend(lua_State *L) {
 	int argc = lua_gettop(L);
@@ -219,7 +217,6 @@ static int lua_loadimgasync(lua_State *L){
 	return 0;
 }
 
-
 static int lua_loadimg(lua_State *L) {
 	int argc = lua_gettop(L);
 	if (argc != 1 && argc != 2) return luaL_error(L, "wrong number of arguments");
@@ -293,7 +290,6 @@ static int lua_drawimg_scale(lua_State *L) {
 	return 0;
 }
 
-
 static int lua_drawimg_part(lua_State *L) {
 	int argc = lua_gettop(L);
 	if (argc != 7 && argc != 8) return luaL_error(L, "wrong number of arguments");
@@ -343,7 +339,6 @@ static int lua_width(lua_State *L) {
 	return 1;
 }
 
-
 static int lua_height(lua_State *L) {
 	int argc = lua_gettop(L);
 	if (argc != 1) return luaL_error(L, "wrong number of arguments");
@@ -360,7 +355,6 @@ static int lua_filters(lua_State *L) {
 	return 0;
 }
 
-
 static int lua_rect(lua_State *L) {
 	int argc = lua_gettop(L);
 	if (argc != 5) return luaL_error(L, "wrong number of arguments");
@@ -374,7 +368,6 @@ static int lua_rect(lua_State *L) {
 
 	return 0;
 }
-
 
 static int lua_line(lua_State *L) {
 	int argc = lua_gettop(L);
@@ -468,7 +461,6 @@ static int lua_circle(lua_State *L) {
 	return 0;
 }
 
-
 static int lua_free(lua_State *L) {
 	int argc = lua_gettop(L);
 #ifndef SKIP_ERROR_HANDLING
@@ -496,7 +488,6 @@ static int lua_free(lua_State *L) {
 	return 0;
 }
 
-
 static int lua_getloadstate(lua_State *L){
 	int argc = lua_gettop(L);
 #ifndef SKIP_ERROR_HANDLING
@@ -519,8 +510,20 @@ static int lua_getloaddata(lua_State *L){
 	}else return 0;
 }
 
+static int lua_load_embedded_png(lua_State *L) {
+	int argc = lua_gettop(L);
+	if (argc != 2) return luaL_error(L, "wrong number of arguments");
+    lua_gc(L, LUA_GCCOLLECT, 0);
+	uint8_t* ptr = (uint8_t *)luaL_checkstring(L, 1);
+	size_t siz = luaL_checkinteger(L, 2);
+	GSTEXTURE* image = NULL;
+	image = loadEmbeddedPNG(ptr, siz, true);
+	lua_pushinteger(L, (uint32_t)(image));
+	return 1;
+}
 //Register our Graphics Functions
 static const luaL_Reg Graphics_functions[] = {
+    {"loadImageEmbedded",         lua_load_embedded_png},
   	{"drawPixel",           		   lua_pixel},
   //{"getPixel",            		  lua_gpixel},
   	{"drawRect",           				lua_rect},
